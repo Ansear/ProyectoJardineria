4a3e17536f44270d7335c0aaa483895fea737898
Ansear <107230475+Ansear@users.noreply.github.com>
2023-11-16 11:45:02 -0500

    feat: :wrench: Add Base Files And Folders

---
+++ BackEnd/API/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public class ApplicationServicesExtension
+    {
+        
+    }
+}
