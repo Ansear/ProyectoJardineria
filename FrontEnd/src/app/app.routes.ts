8ebeda9771aa2d5d800db70040502bb561bae753
DeLxCruz <lopezsantiago3503@gmail.com>
2024-02-27 17:50:24 -0500

    feat: :sparkles: Begin Sidebar

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./auth/auth.routes').then(m => m.Auth_Routes)
+  },
+  {
+    path: 'sidebar',
+    component: SidebarComponent
   }
 ];
